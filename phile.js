0160e497f47dce061ea1341c44b1b15056be1b79
Nyx <n@0x2a.re>
2022-04-10 12:53:08 +0100

    fixed wrong redirect localtion

--- phile.js
+++ phile.js
@@ -476,7 +476,7 @@ recover().then(() => {
 // https redirect server
 const redirectServer = http.createServer((req, res) => {
     res.writeHead(301, {
-        "Location": `https://${req.url}`
+        "Location": `https://${req.host}`
     });
     res.end();
 });
